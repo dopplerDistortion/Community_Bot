d226edf48b372b4b3c218f727286aaa6244888ca
Göksenin Güngör <goksenin.gngr@gmail.com>
2017-05-31 01:04:03 +0300

    Demo

--- Giphy_Random_Post_2_Facebook_Page.py
+++ Giphy_Random_Post_2_Facebook_Page.py
@@ -12,17 +12,18 @@ ss = int(now1.second) + 1
 
 def todayAt(hr, min=0, sec=0, micros=0):
     now = datetime.datetime.now()
-    return now.replace(hour=hr, minute=min, second=sec, microsecond=micros) 
+    return now.replace(hour=hr, minute=min, second=sec, microsecond=micros)
 
 def post():
   g = safygiphy.Giphy()
-  r = g.random(tag="robot")
+  r = g.random(tag="design")
   img = r["data"]["image_original_url"]
 
   # Fill in the values noted in previous steps here
   cfg = {
-    "page_id"      : "",  # page id in about page
-    "access_token" : ""   # access token from graph api
+    "page_id"      : "1010368365728206",  # page id in about page
+    #"access_token" : "EAANtVYu0l2EBAP7Dcknx1SeZCloQMoZC1B5AILAeDcGKsv3S15ZC1VD$
+    "access_token" : "EAANtVYu0l2EBAAFtXqP7iNxLUT680w2lnYCCpL7FmUxwDTvz9YnI6X7aH2KkCzkMgHzPvz3tQhSeZBJMYOQjUmp4NSBi04x2o9K6ySLm5JlZBUdnu7UBtylUta1KbeeZBbGVGnt2CC9ZCOkBrOmFArMSErOCuBhNkSm4VOfn3wZDZD"
     }
 
   api = get_api(cfg)
@@ -33,7 +34,7 @@ def post():
      'description': '',
      'picture': img
 }
-  msg = "Hello World! #randomposts" #post
+  msg = ":D #random #giphy" #post
   status = api.put_wall_post(message=msg, attachment=atc)
 
 def get_api(cfg):
@@ -42,13 +43,6 @@ def get_api(cfg):
 
 
 if __name__ == "__main__":
-  while True:
-    timeNow = datetime.datetime.now()
-    print timeNow.hour, s, timeNow.minute, d
-    if timeNow >= todayAt (s):
-        print "Posted"
-        s += 1
-        if s >= 23:
-            s = 0
-        post()
-        
+  post()
+  print "posted"
+  exit(0)
